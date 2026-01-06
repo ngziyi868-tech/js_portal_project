8a0fcb3da25d84dec9fea96f0f9148aa8eefa56d
Ziyi <ngziyi868@gmail.com>
2026-01-05 19:48:41 -0700

    starting

---
+++ cards.js
@@ -0,0 +1,6 @@
+let cardsList = document.querySelector(".cards");
+let cards = [];
+
+
+// look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
+// Look up how to push an object to an array
